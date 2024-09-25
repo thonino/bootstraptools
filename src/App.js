9600ed1d1739db6e9c33a15d067bf48a917002f5
thonino <thoninoben@gmail.com>
2024-09-25 17:54:46 +0200

    daily

--- src/App.js
+++ src/App.js
@@ -46,6 +46,9 @@ function App() {
     ); 
   };
 
+  // Toast 
+
+
 
 
   return (
@@ -86,20 +89,66 @@ function App() {
         <button className="btn btn-success">Add</button>
       </form>
 
-      <h1 className="mt-2"> Progress bar </h1>
+      <h1 className="mt-2 text-success"> alert </h1>
+      <div class="alert alert-primary container" role="alert">
+        A simple primary alert !
+      </div>
+
+      <h1 className="mt-2 text-success"> Spinner </h1>
+      <div className="spinner-border text-success" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+
+      <h1 className="mt-2 text-warning"> Button group</h1>
+      <div className="btn-group" role="group" aria-label="Basic example">
+        <button type="button" className="btn btn-success">
+          Left
+        </button>
+        <button type="button" className="btn btn-warning">
+          Middle
+        </button>
+        <button type="button" className="btn btn-danger">
+          Right
+        </button>
+      </div>
+
+      <h1 className="mt-2 text-primary"> Progress bar </h1>
       <div
-        class="progress mx-4"
+        className="progress mx-4"
         role="progressbar"
         aria-label="Basic example"
         aria-valuenow="75"
         aria-valuemin="0"
         aria-valuemax="100"
       >
-        <div class={`progress-bar w-${progress}`}></div>
+        <div className={`progress-bar w-${progress}`}></div>
       </div>
       <button onClick={handleAdd25} className="btn btn-primary mt-2">
         Add 25
       </button>
+
+      <h1 className="mt-2 text-primary"> Card </h1>
+      <div className="d-flex justify-content-center mb-4">
+        <div className="card w-25 ">
+          <img
+            src="https://cdn.pixabay.com/photo/2023/10/14/09/19/meditation-8314420_960_720.png"
+            className="card-img-top d"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+
+      
     </div>
   );
 }
